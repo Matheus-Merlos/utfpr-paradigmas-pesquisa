73794e2e9a4954180c829904a47c3d89e1cb4e70
Matheus-Merlos <matheusazrmerlos@gmail.com>
2026-03-28 16:14:41 -0300

    feat: modularizar a função de pesquisa

--- main.go
+++ main.go
@@ -1,82 +1,13 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
-	"path/filepath"
-	"strings"
-	"sync"
+	"utfpr-paradigmas-pesquisa/scanner"
 )
 
-func HasIndex(path string, searchIndex string) bool {
-	file, err := os.Open(path)
-	if err != nil {
-		return false
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	lowerCaseSearchIndex := strings.ToLower(searchIndex)
-
-	for scanner.Scan() {
-		line := scanner.Text()
-		if strings.Contains(strings.ToLower(line), lowerCaseSearchIndex) {
-			return true
-		}
-	}
-
-	return false
-}
-
-func GetAllFilesFromPath(path string, searchIndex string, results chan<- string, wg *sync.WaitGroup) {
-	defer wg.Done()
-
-	entries, err := os.ReadDir(path)
-	if err != nil {
-		fmt.Println("Erro ao ler diretório: ", err)
-		return
-	}
-
-	for _, entry := range entries {
-		fullPath := filepath.Join(path, entry.Name())
-		if entry.IsDir() {
-			wg.Add(1)
-			go GetAllFilesFromPath(fullPath, searchIndex, results, wg)
-		} else if entry.Type().IsRegular() {
-			absolutePath, err := filepath.Abs(fullPath)
-			if err != nil {
-				fmt.Println("Erro ao ler caminho absoluto: ", err)
-				return
-			}
-
-			if HasIndex(absolutePath, searchIndex) {
-				results <- absolutePath
-			}
-		}
-	}
-}
-
 func main() {
-	fmt.Println("Lendo diretório atual...")
-	var wg sync.WaitGroup
-
-	results := make(chan string)
-
-	wg.Add(1)
-
-	go GetAllFilesFromPath(".", os.Args[1], results, &wg)
-
-	go func() {
-		wg.Wait()
-		close(results)
-	}()
-
-	var foundPaths []string
-
-	for path := range results {
-		foundPaths = append(foundPaths, path)
-	}
+	foundPaths := scanner.ScanFilesForIndex(".", os.Args[1])
 
 	fmt.Printf("Busca conluida, total de pastas encontradas: %d\n", len(foundPaths))
 
---
+++ scanner/scanner.go
@@ -0,0 +1,82 @@
+package scanner
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
+)
+
+func HasIndex(path string, searchIndex string) bool {
+	file, err := os.Open(path)
+	if err != nil {
+		return false
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	lowerCaseSearchIndex := strings.ToLower(searchIndex)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.Contains(strings.ToLower(line), lowerCaseSearchIndex) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func StreamFilesWithIndex(path string, searchIndex string, results chan<- string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	entries, err := os.ReadDir(path)
+	if err != nil {
+		fmt.Println("Erro ao ler diretório: ", err)
+		return
+	}
+
+	for _, entry := range entries {
+		fullPath := filepath.Join(path, entry.Name())
+		if entry.IsDir() {
+			wg.Add(1)
+			go StreamFilesWithIndex(fullPath, searchIndex, results, wg)
+		} else if entry.Type().IsRegular() {
+			absolutePath, err := filepath.Abs(fullPath)
+			if err != nil {
+				fmt.Println("Erro ao ler caminho absoluto: ", err)
+				return
+			}
+
+			if HasIndex(absolutePath, searchIndex) {
+				results <- absolutePath
+			}
+		}
+	}
+}
+
+func ScanFilesForIndex(path string, searchIndex string) []string {
+	fmt.Println("Lendo diretório atual...")
+	var wg sync.WaitGroup
+
+	results := make(chan string)
+
+	wg.Add(1)
+
+	go StreamFilesWithIndex(path, searchIndex, results, &wg)
+
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
+	var foundPaths []string
+
+	for path := range results {
+		foundPaths = append(foundPaths, path)
+	}
+
+	return foundPaths
+}
