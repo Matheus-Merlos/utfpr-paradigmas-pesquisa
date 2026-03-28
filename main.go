bbb6cc043f8461682d18228716d2b0890b48b0b8
Matheus-Merlos <matheusazrmerlos@gmail.com>
2026-03-28 17:52:01 -0300

    feat: fazer o usuário poder escolher entre ler do banco e ler novamente (sistema de caching e indexação)

--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
-	"super-grep/db"
 	"super-grep/scanner"
+	db "super-grep/storage"
 )
 
 func main() {
@@ -19,23 +19,38 @@ func main() {
 
 	history, err := db.FindTerm(searchIndex)
 	if err == nil && len(history) > 0 {
-		fmt.Printf("O termo já existe no banco com %d ocorrencias passsadas.", len(history))
+		fmt.Printf("O termo já existe no banco com %d ocorrencias passsadas.\n", len(history))
 	}
 
-	fmt.Printf("Iniciando busca paralela por %s\n", searchIndex)
-	foundPaths := scanner.ScanFilesForIndex(".", searchIndex)
+	var choice string
+	var foundPaths []string
+	if len(history) > 0 {
+		fmt.Println("Você gostaria de buscar novamente?")
+		fmt.Print("Sua escolha [y/n(default)]: ")
+		fmt.Scanln(&choice)
+	}
 
-	fmt.Printf("Busca conluida, total de ocorrências encontradas agora: %d\n", len(foundPaths))
+	if choice == "y" || len(history) == 0 {
+		fmt.Printf("Iniciando busca paralela por %s\n", searchIndex)
+		foundPaths = scanner.ScanFilesForIndex(".", searchIndex)
 
-	if len(foundPaths) > 0 {
-		err := db.UpsertPath(searchIndex, foundPaths)
-		if err != nil {
-			fmt.Println("Erro ao escrever no banco de dados: ", err)
-			return
+		fmt.Printf("Busca concluida, total de ocorrências encontradas agora: %d\n", len(foundPaths))
+
+		if len(foundPaths) > 0 {
+			err := db.UpsertPath(searchIndex, foundPaths)
+			if err != nil {
+				fmt.Println("Erro ao escrever no banco de dados: ", err)
+				return
+			}
+			fmt.Println("Índice do banco de dados atualizado com sucesso.")
+		} else {
+			fmt.Println("Nenhuma ocorrência nova para salvar no índice.")
 		}
-		fmt.Println("Índice do banco de dados atualizado com sucesso.")
 	} else {
-		fmt.Println("Nenhuma ocorrência nova para salvar no índice.")
+		fmt.Println("Carregando resultados do banco de dados...")
+		for _, item := range history {
+			foundPaths = append(foundPaths, item.AbsolutePathLine)
+		}
 	}
 
 	fmt.Println("Resultados:")
